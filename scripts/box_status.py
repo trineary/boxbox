8743ff24b90023a86bbdd81c77e4b617650c7986
Patrick Neary <patrick.neary@sandboxquantum.com>
2022-11-11 11:45:09 -0700

    -- lat/lon plotting kind of working.  Map zooms in super far at the beginning.

--- scripts/box_status.py
+++ scripts/box_status.py
@@ -3,9 +3,11 @@ import sys
 dir_path = os.getcwd()
 sys.path.append(dir_path)
 
+import json
 import datetime
 from time import sleep
 import numpy as np
+import pandas as pd
 import streamlit as st
 # import yaml
 import asyncio
@@ -13,7 +15,7 @@ import asyncio
 from scripts.mqtt import get_mqtt_client
 from scripts.helpers import get_config
 
-
+boxbox_dict = None
 
 def st_print(text):
     st.text(text)
@@ -21,7 +23,9 @@ def st_print(text):
 
 # print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
-    st_print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    global boxbox_dict
+    # st_print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    boxbox_dict = str(msg.payload.decode('ascii'))
 
 
 # print which topic was subscribed to
@@ -45,18 +49,57 @@ def on_publish(client, userdata, mid):
     st_print("mid: " + str(mid))
 
 
-async def mqtt_periodic(test):
-    # col1, col2, col3 = st.columns(3)
-    # col1.metric("Uptime", "0")
-    # col2.metric("Wind", "9 mph", "-8%")
-    # col3.metric("Humidity", "86%", "4%")
+async def mqtt_periodic():  # test, cols, strt_map, anom_map):
+    global boxbox_dict
+    empty_cols = st.empty()
+    col1, col2, col3, col4, col5 = empty_cols.columns(5)
+    col1.metric("VMR Cnt", "0")
+    col2.metric("MFAM Cnt", "0")
+    col3.metric("VN3xx Cnt", "0")
+    col4.metric("Sat Cnt", "0")
+    col5.metric("Uptime", "0")
+
+    strt_map = st.empty()
     start_time = datetime.datetime.now()
+    test = st.empty()
+    dict_str = st.empty()
+    lat_lons = None
+
     while True:
         if "mqtt_client" in st.session_state:
             st.session_state.mqtt_client.loop(1.0)
-            test.text(f"Uptime: {int((datetime.datetime.now() - start_time).seconds)} secs")
+            test.text(f"Uptime loop: {int((datetime.datetime.now() - start_time).seconds)} secs")
+            if boxbox_dict is not None:
+                boxbox_dict = boxbox_dict.replace("'", '"')
+                dict_str.text(f"boxbox_dict: {boxbox_dict}")
+                st.text(f"box tye: {type(boxbox_dict)}")
+                box_data = json.loads(boxbox_dict)
+
+                # Update data in columns
+                col1, col2, col3, col4, col5 = empty_cols.columns(5)
+                col1.metric("VMR Cnt", box_data["VMR_PUB_Cnt"])
+                col2.metric("MFAM Cnt", box_data["VN3xx_PUB_Cnt"])
+                col3.metric("VN3xx Cnt", box_data["MFAM_PUB_Cnt"])
+                col4.metric("Sat Cnt", box_data["num_sats"])
+                col5.metric("Uptime", box_data["up_time"])
+
+                # Plot lat/lon
+                lat, lon = float(box_data['lat']), float(box_data['long'])
+                if lat_lons is None:
+                    lat_lons = np.array([[lat, lon]])
+                    lat_lons = np.append(lat_lons, [[lat, lon]], axis=0)
+                    lat_lons = np.append(lat_lons, [[lat, lon]], axis=0)
+                else:
+                    lat_lons = np.append(lat_lons, [[lat, lon]], axis=0)
+
+                st.write(f'np: {lat_lons.shape}')
+                df = pd.DataFrame(lat_lons, columns=['lat', 'lon'])
+                st.write(f'df: {df.shape}')
+                strt_map.map(df)
+
+                boxbox_dict = None
         else:
-            test.text(f"Uptime: {int((datetime.datetime.now() - start_time).seconds)} secs")
+            test.text(f"Uptime  else: {int((datetime.datetime.now() - start_time).seconds)} secs")
             sleep(1.0)
 
 
@@ -107,8 +150,11 @@ if re_init_mqtt is True and box_select is not None:
 st_print("pre asyncio")
 
 
-test = st.empty()
-test.text("Test text")
-asyncio.run(mqtt_periodic(test))
+# test = st.empty()
+# cols = st.empty()
+# strt_map = st.empty()
+# anom_map = st.empty()
+# test.text("Test text")
+asyncio.run(mqtt_periodic())  # test, cols, strt_map, anom_map))
 st_print("post asyncio")
 
